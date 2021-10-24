609a4e9eaaebc189ad4b528cf22309445896f852
a.bertanzon <a.bertanzon@ferlini.it>
2021-10-24 09:57:28 +0200

    Added LocalInfo
    Added the local info entity and table that keeps tracks of the last time fetched and knows when to fetch

---
+++ app/src/main/java/com/abcode/asteroidradar/data/LocalDao.kt
@@ -0,0 +1,4 @@
+package com.abcode.asteroidradar.data
+
+class LocalDao {
+}
---
+++ app/src/main/java/com/abcode/asteroidradar/repository/LocalInfoRepository.kt
@@ -0,0 +1,86 @@
+package com.abcode.asteroidradar.repository
+
+import android.util.Log
+import com.abcode.asteroidradar.Asteroid
+import com.abcode.asteroidradar.BuildConfig
+import com.abcode.asteroidradar.PictureOfDay
+import com.abcode.asteroidradar.api.AsteroidsApi
+import com.abcode.asteroidradar.api.getNextSevenDaysFormattedDates
+import com.abcode.asteroidradar.api.parseAsteroidsJsonResult
+import com.abcode.asteroidradar.data.AsteroidsDatabase
+import com.abcode.asteroidradar.data.asDomainModel
+import com.abcode.asteroidradar.data.toDtoModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import javax.inject.Inject
+
+
+class AsteroidRepository @Inject constructor(
+    private val api: AsteroidsApi,
+    private val db: AsteroidsDatabase
+) {
+
+    fun getAsteroids(filter: FilterEnum = FilterEnum.ALL): Flow<List<Asteroid>> {
+        return when (filter) {
+            FilterEnum.ALL -> {
+                db.asteroidDao().getAllAsteroids().map {
+                    it.asDomainModel()
+                }
+            }
+            FilterEnum.WEEK -> {
+                db.asteroidDao().getWeeklyAsteroids().map {
+                    it.asDomainModel()
+                }
+            }
+            else -> {
+                db.asteroidDao().getTodayAsteroids().map {
+                    it.asDomainModel()
+                }
+            }
+        }
+    }
+
+    suspend fun refreshAsteroids(): Boolean {
+        var output: Boolean
+
+        withContext(Dispatchers.IO) {
+            output = try {
+                val nextSevenDays = getNextSevenDaysFormattedDates()
+                val asteroids = api.getAsteroidsAsync(
+                    startDate = nextSevenDays[0],
+                    nextSevenDays[6],
+                    BuildConfig.ASTEROID_API_KEY
+                )
+
+                val stringAsteroid = asteroids.string()
+                //Log.i("ASTEROIDSCALL", stringAsteroid)
+
+                val asteroidsToInsert = parseAsteroidsJsonResult(JSONObject(stringAsteroid))
+                db.asteroidDao().insertAsteroids(asteroidsToInsert.toDtoModel())
+                true
+            } catch (err: Throwable) {
+                Log.e(
+                    "AsteroidRepository",
+                    err.localizedMessage ?: "Error retrieving data from API"
+                )
+                false
+            }
+        }
+        return output
+    }
+
+    suspend fun refreshPictureOfDay(): PictureOfDay? =
+        withContext(Dispatchers.IO) {
+            val output = try {
+                api.getPictureOfDayAsync(BuildConfig.ASTEROID_API_KEY)
+            } catch (err: Exception) {
+                Log.e("refreshPictureOfDay", err.printStackTrace().toString())
+                null
+            }
+            return@withContext output
+        }
+}
+
